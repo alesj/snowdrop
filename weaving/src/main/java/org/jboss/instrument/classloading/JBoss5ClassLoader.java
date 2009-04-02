6b8c3261bef2c1ef40ad67e21250bd13f68d4749
Ales Justin <ales.justin@gmail.com>
2009-04-02 21:56:09 +0000

    Use more optimized version in JBossAS5.1.

--- weaving/src/main/java/org/jboss/instrument/classloading/JBoss5ClassLoader.java
+++ weaving/src/main/java/org/jboss/instrument/classloading/JBoss5ClassLoader.java
@@ -27,12 +27,7 @@ import java.security.AccessController;
 import java.security.PrivilegedExceptionAction;
 
 import org.jboss.classloader.spi.ClassLoaderPolicy;
-import org.jboss.classloader.spi.ClassLoaderSystem;
 import org.jboss.classloader.spi.base.BaseClassLoader;
-import org.jboss.classloader.spi.base.BaseClassLoaderDomain;
-import org.jboss.classloader.spi.base.BaseClassLoaderPolicy;
-import org.jboss.classloader.spi.base.BaseClassLoaderSystem;
-import org.jboss.logging.Logger;
 import org.jboss.util.loading.Translator;
 import org.springframework.util.Assert;
 
@@ -43,18 +38,13 @@ import org.springframework.util.Assert;
  *
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
-public class JBoss5ClassLoader
+public abstract class JBoss5ClassLoader extends ReflectionHelper
 {
-   private static Logger log = Logger.getLogger(JBoss5ClassLoader.class);
-
    private final BaseClassLoader classLoader;
-
    private ClassLoaderPolicy policy;
-   private Method addTranslator;
-   private ClassLoaderSystem system;
 
    @SuppressWarnings("unchecked")
-   public JBoss5ClassLoader(BaseClassLoader classLoader)
+   protected JBoss5ClassLoader(BaseClassLoader classLoader)
    {
       Assert.notNull(classLoader, "ClassLoader must not be null");
       this.classLoader = classLoader;
@@ -74,6 +64,16 @@ public class JBoss5ClassLoader
    }
 
    /**
+    * Get the policy.
+    *
+    * @return the policy
+    */
+   protected ClassLoaderPolicy getPolicy()
+   {
+      return policy;
+   }
+
+   /**
     * Do instantiate method, variables.
     *
     * @throws Exception for any error
@@ -82,84 +82,31 @@ public class JBoss5ClassLoader
    {
       Method getPolicy = getMethod(BaseClassLoader.class, "getPolicy");
       policy = invokeMethod(getPolicy, classLoader, ClassLoaderPolicy.class);
-      try
-      {
-         // let's check if we have a patched policy, with translator per policy
-         addTranslator = getMethod(BaseClassLoaderPolicy.class, "addTranslator");
-      }
-      catch (Exception ignored)
-      {
-         log.info("Policy doesn't have setTranslator, falling back to ClassLoaderSystem.");
-
-         Method getClassLoaderDomain = getMethod(BaseClassLoaderPolicy.class, "getClassLoaderDomain");
-         BaseClassLoaderDomain domain = invokeMethod(getClassLoaderDomain, policy, BaseClassLoaderDomain.class);
-         Method getClassLoaderSystem = getMethod(BaseClassLoaderDomain.class, "getClassLoaderSystem");
-         BaseClassLoaderSystem system = invokeMethod(getClassLoaderSystem, domain, BaseClassLoaderSystem.class);
-         if (system instanceof ClassLoaderSystem)
-         {
-            this.system = ClassLoaderSystem.class.cast(system);
-         }
-         else
-         {
-            throw new IllegalArgumentException("ClassLoaderSyatem must be instance of [" + ClassLoaderSystem.class.getName() + "]");
-         }
-      }
+      fallbackStrategy();
    }
 
    /**
-    * Get method from class.
+    * The fallback strategy.
     *
-    * @param clazz the owner class
-    * @param name the method name
-    * @return declared method
     * @throws Exception for any error
     */
-   private static Method getMethod(Class<?> clazz, String name) throws Exception
+   protected void fallbackStrategy() throws Exception
    {
-      Method method = clazz.getDeclaredMethod(name);
-      method.setAccessible(true);
-      return method;
-   }
-
-   /**
-    * Invoke method and check the result.
-    *
-    * @param method the method
-    * @param target the target
-    * @param expectedType the expected type
-    * @param <T> the exact type
-    * @return invocation's result
-    * @throws Exception for any error
-    */
-   private static <T> T invokeMethod(Method method, Object target, Class<T> expectedType) throws Exception
-   {
-      Object result = method.invoke(target);
-      if (expectedType.isInstance(result) == false)
-         throw new IllegalArgumentException("Returned result must be instance of [" + expectedType.getName() + "]");
-
-      return expectedType.cast(result);
    }
 
    public void addTransformer(ClassFileTransformer transformer)
    {
       Assert.notNull(transformer, "ClassFileTransformer must not be null");
       Translator translator = new ClassFileTransformer2Translator(transformer);
-      if (addTranslator != null)
-      {
-         try
-         {
-            addTranslator.invoke(translator);
-         }
-         catch (Exception e)
-         {
-            throw new IllegalArgumentException(e);
-         }
-      }
-      else
-      {
-         system.setTranslator(translator);   
-      }
+      addTranslator(translator);
    }
+
+   /**
+    * Add the translator.
+    *
+    * @param translator the translator
+    */
+   protected abstract void addTranslator(Translator translator);
 
    public ClassLoader getInternalClassLoader()
    {
--- weaving/src/main/java/org/jboss/instrument/classloading/JBoss5LoadTimeWeaver.java
+++ weaving/src/main/java/org/jboss/instrument/classloading/JBoss5LoadTimeWeaver.java
@@ -22,6 +22,7 @@
 package org.jboss.instrument.classloading;
 
 import java.lang.instrument.ClassFileTransformer;
+import java.lang.reflect.Method;
 
 import org.jboss.classloader.spi.base.BaseClassLoader;
 import org.springframework.instrument.classloading.LoadTimeWeaver;
@@ -33,7 +34,7 @@ import org.springframework.util.ClassUtils;
  *
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
-public class JBoss5LoadTimeWeaver implements LoadTimeWeaver
+public class JBoss5LoadTimeWeaver extends ReflectionHelper implements LoadTimeWeaver
 {
    private JBoss5ClassLoader classLoader;
 
@@ -51,7 +52,49 @@ public class JBoss5LoadTimeWeaver implements LoadTimeWeaver
          throw new IllegalArgumentException(classLoader + " and its parents are not suitable ClassLoaders: " +
                "An [" + BaseClassLoader.class.getName() + "] implementation is required.");
       }
-      this.classLoader = new JBoss5ClassLoader(bcl);
+      this.classLoader = createJBoss5ClassLoader(bcl);
+   }
+
+   /**
+    * Create a JBoss5 classloader wrapper
+    * based on the underlying JBossAS version.
+    *
+    * @param bcl the base classloader
+    * @return new JBoss5 classloader wrapper
+    */
+   protected JBoss5ClassLoader createJBoss5ClassLoader(BaseClassLoader bcl)
+   {
+      int versionNumber = 0;
+      String tag;
+      
+      try
+      {
+         // BCL should see Version class
+         Class<?> versionClass = bcl.loadClass("org.jboss.Version");
+         Method getInstance = getMethod(versionClass, "getInstance");
+         Object version = getInstance.invoke(null); // static method
+
+         Method getMajor = getMethod(versionClass, "getMajor");
+         versionNumber += 100 * invokeMethod(getMajor, version, Integer.class);
+         Method getMinor = getMethod(versionClass, "getMinor");
+         versionNumber += 10 * invokeMethod(getMinor, version, Integer.class);
+         Method getRevision = getMethod(versionClass, "getRevision");
+         versionNumber += invokeMethod(getRevision, version, Integer.class);
+         Method getTag = getMethod(versionClass, "getTag");
+         tag = invokeMethod(getTag, version, String.class);
+      }
+      catch (Exception e)
+      {
+         log.warn("Exception creating JBoss5 CL wrapper: " + e + ", falling back to JBoss50ClassLoader wrapper.");
+         return new JBoss50ClassLoader(bcl);
+      }
+
+      if (versionNumber < 500) // this only works on new MC code
+         throw new IllegalArgumentException("JBoss5LoadTimeWeaver can only be used on new JBoss Microcontainer ClassLoader.");
+      else if (versionNumber <= 501 || (versionNumber == 510 && "Beta1".equals(tag)))
+         return new JBoss50ClassLoader(bcl);
+      else
+         return new JBoss51ClassLoader(bcl);
    }
 
    /**
