a2c276033e65eab020b2353cd13a19b5f1c86b93
Marius Bogoevici <marius.bogoevici@gmail.com>
2009-04-30 03:46:26 +0000

    Adding an XmlWebApplicationContext subclass for solving pattern-based resource search and classpath scanning.

---
+++ deployers/src/main/java/org/jboss/spring/factory/VFSXmlWebApplicationContext.java
@@ -0,0 +1,61 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.spring.factory;
+
+import org.jboss.spring.vfs.VFSResourceLoader;
+import org.jboss.spring.vfs.VFSServletContextResourcePatternResolver;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
+
+/**
+ * XmlWebApplicationContext variant adding support for classpath
+ * scanning in JBoss AS 5, using the VFS file system.
+ *
+ * @author <a href="mailto:mariusb@redhat.com">Marius Bogoevici</a>
+ */
+public class VFSXmlWebApplicationContext extends XmlWebApplicationContext
+{
+
+   @Override
+   protected ResourcePatternResolver getResourcePatternResolver()
+   {
+      return new VFSServletContextResourcePatternResolver(
+              new WebApplicationContextAwareVFSResourceLoader());
+   }
+
+   /**
+    * Customization of {@link VFSResourceLoader} that delegates to the owner class
+    * (i.e. XmlWebApplicationContext) for retrieving resources by path, allowing
+    * for ServletContextResources to be returned in this case.
+    */
+   private class WebApplicationContextAwareVFSResourceLoader extends VFSResourceLoader
+   {
+      @Override
+      protected Resource getResourceByPath(String path)
+      {
+         return VFSXmlWebApplicationContext.this.getResourceByPath(path);
+      }
+   }
+}
+
