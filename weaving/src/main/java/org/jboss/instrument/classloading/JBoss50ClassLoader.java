fdf0ea0a5902f5e15e8dc547a83212ee761e9da8
Ales Justin <ales.justin@gmail.com>
2009-05-14 18:56:08 +0000

    Suppress deprecation.

--- weaving/src/main/java/org/jboss/instrument/classloading/JBoss50ClassLoader.java
+++ weaving/src/main/java/org/jboss/instrument/classloading/JBoss50ClassLoader.java
@@ -88,6 +88,7 @@ public class JBoss50ClassLoader extends JBoss5ClassLoader
       }
       else
       {
+         //noinspection deprecation
          system.setTranslator(translator);
       }
    }
