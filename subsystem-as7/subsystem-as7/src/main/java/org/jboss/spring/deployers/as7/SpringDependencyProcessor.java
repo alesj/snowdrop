59fe5cb4d09165fe2311aa1864ebc11109ee3e6d
Marius Bogoevici <marius.bogoevici@gmail.com>
2011-09-24 12:38:46 -0400

    Basic support for separating a Spring module from the AS7 subsystem

---
+++ subsystem-as7/subsystem-as7/src/main/java/org/jboss/spring/deployers/as7/SpringDependencyProcessor.java
@@ -0,0 +1,82 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.spring.deployers.as7;
+
+import org.jboss.as.server.deployment.Attachments;
+import org.jboss.as.server.deployment.DeploymentPhaseContext;
+import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
+import org.jboss.as.server.deployment.DeploymentUnitProcessor;
+import org.jboss.as.server.deployment.module.ModuleDependency;
+import org.jboss.as.server.deployment.module.ModuleSpecification;
+import org.jboss.modules.Module;
+import org.jboss.modules.ModuleIdentifier;
+import org.jboss.modules.filter.PathFilters;
+
+/**
+ * @author Marius Bogoevici
+ */
+public class SpringDependencyProcessor implements DeploymentUnitProcessor {
+
+    private static final ModuleIdentifier MODULE_IDENTIFIER_SNOWDROP = ModuleIdentifier.create("org.jboss.snowdrop");
+    private static final ModuleIdentifier MODULE_IDENTIFIER_SPRING = ModuleIdentifier.create("org.springframework.spring");
+
+    @Override
+    public void deploy(DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
+        final DeploymentUnit deploymentUnit = phaseContext.getDeploymentUnit();
+        SpringDeployment locations = SpringDeployment.retrieveFrom(deploymentUnit);
+        if (locations == null || locations.getContextDefinitionLocations().isEmpty()) {
+            return;
+        }
+
+        ModuleSpecification moduleSpecification = deploymentUnit.getAttachment(Attachments.MODULE_SPECIFICATION);
+
+        addDependency(MODULE_IDENTIFIER_SNOWDROP, moduleSpecification);
+
+//        boolean hasSpringDependency = false;
+//        for (ModuleDependency moduleDependency : moduleSpecification.getAllDependencies()) {
+//            if (moduleDependency.getIdentifier().equals(MODULE_IDENTIFIER_SPRING)) {
+//                hasSpringDependency = true;
+//                break;
+//            }
+//        }
+//        if (!hasSpringDependency) {
+//
+//        }
+        addDependency(MODULE_IDENTIFIER_SPRING, moduleSpecification);
+
+        deploymentUnit.addToAttachmentList(Attachments.ADDITIONAL_ANNOTATION_INDEXES, MODULE_IDENTIFIER_SNOWDROP);
+    }
+
+    private void addDependency(ModuleIdentifier moduleIdentifier, ModuleSpecification moduleSpecification) {
+        ModuleDependency moduleDependency = new ModuleDependency(Module.getBootModuleLoader(), moduleIdentifier, false, false, true);
+        moduleDependency.addExportFilter(PathFilters.getMetaInfFilter(), true);
+        moduleDependency.addImportFilter(PathFilters.getMetaInfFilter(), true);
+        moduleSpecification.addUserDependency(moduleDependency);
+    }
+
+    @Override
+    public void undeploy(DeploymentUnit context) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ subsystem-as7/subsystem-as7/src/main/java/org/jboss/spring/deployers/as7/SpringStructureProcessor.java
@@ -0,0 +1,74 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.spring.deployers.as7;
+
+import org.jboss.as.server.deployment.Attachments;
+import org.jboss.as.server.deployment.DeploymentPhaseContext;
+import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.as.server.deployment.DeploymentUnitProcessingException;
+import org.jboss.as.server.deployment.DeploymentUnitProcessor;
+import org.jboss.as.server.deployment.module.ResourceRoot;
+import org.jboss.logging.Logger;
+import org.jboss.vfs.VirtualFile;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Marius Bogoevici
+ */
+public class SpringStructureProcessor implements DeploymentUnitProcessor {
+
+    private static final Logger log = Logger.getLogger("org.jboss.snowdrop");
+
+    @Override
+    public void deploy(DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
+        DeploymentUnit deploymentUnit = phaseContext.getDeploymentUnit();
+
+        ResourceRoot deploymentRoot = deploymentUnit.getAttachment(Attachments.DEPLOYMENT_ROOT);
+
+        if (deploymentRoot == null) {
+            return;
+        }
+
+        Set<VirtualFile> springContextLocations = new HashSet<VirtualFile>();
+        VirtualFile metaInf = deploymentRoot.getRoot().getChild("META-INF");
+        for (VirtualFile child : metaInf.getChildren()) {
+            if (child.getName().endsWith("-spring.xml")) {
+                springContextLocations.add(child);
+                log.debug("Found:" + child.getPathName());
+            }
+        }
+
+        if (!springContextLocations.isEmpty()) {
+            SpringDeployment springDeployment = new SpringDeployment(springContextLocations);
+            springDeployment.attachTo(deploymentUnit);
+        }
+
+    }
+
+    @Override
+    public void undeploy(DeploymentUnit context) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
