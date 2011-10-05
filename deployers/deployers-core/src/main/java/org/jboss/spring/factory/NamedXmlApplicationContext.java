2253ef2bca2d5491bf812532e194bb8313260a52
Marius Bogoevici <marius.bogoevici@gmail.com>
2011-10-05 00:30:07 -0400

    Extract name separately from file

--- deployers/deployers-core/src/main/java/org/jboss/spring/factory/NamedXmlApplicationContext.java
+++ deployers/deployers-core/src/main/java/org/jboss/spring/factory/NamedXmlApplicationContext.java
@@ -21,15 +21,23 @@
  */
 package org.jboss.spring.factory;
 
-import java.io.IOException;
-
 import org.jboss.spring.vfs.context.VFSClassPathXmlApplicationContext;
-
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.ResourceEntityResolver;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
+import org.xml.sax.InputSource;
+
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author <a href="mailto:ales.justin@genera-lynx.com">Ales Justin</a>
@@ -42,6 +50,8 @@ public class NamedXmlApplicationContext extends VFSClassPathXmlApplicationContex
 
     private NamedXmlBeanDefinitionReader beanDefinitionReader;
 
+    private String name;
+
     public NamedXmlApplicationContext(String defaultName, Resource resource) throws BeansException {
         this(defaultName, resource, true);
     }
@@ -51,6 +61,7 @@ public class NamedXmlApplicationContext extends VFSClassPathXmlApplicationContex
         super(new String[]{}, false);
         this.defaultName = defaultName;
         this.resource = resource;
+        initializeNames(resource);
         if (refresh) {
             refresh();
         }
@@ -80,12 +91,46 @@ public class NamedXmlApplicationContext extends VFSClassPathXmlApplicationContex
     }
 
     public String getName() {
-        String name = beanDefinitionReader.getName() != null ? beanDefinitionReader.getName() : defaultName;
-        if (name == null) {
-            throw new IllegalArgumentException("Bean factory JNDI name must be set!");
-        }
-
         return name;
     }
 
+    private void initializeNames(Resource resource) {
+        try {
+            XPath xPath = XPathFactory.newInstance().newXPath();
+            xPath.setNamespaceContext(new NamespaceContext() {
+                @Override
+                public String getNamespaceURI(String prefix) {
+                    return "http://www.springframework.org/schema/beans";
+                }
+
+                @Override
+                public String getPrefix(String namespaceURI) {
+                    return "beans";
+                }
+
+                @Override
+                public Iterator getPrefixes(String namespaceURI) {
+                    return Collections.singleton("beans").iterator();
+                }
+            });
+            String expression = "/beans:beans/beans:description";
+            InputSource inputSource = new InputSource(resource.getInputStream());
+            String description = xPath.evaluate(expression, inputSource);
+            if (description != null) {
+                Matcher bfm = Pattern.compile(NamedXmlBeanDefinitionParser.BEAN_FACTORY_ELEMENT).matcher(description);
+                if (bfm.find()) {
+                    this.name = bfm.group(1);
+                }
+                //                Matcher pbfm = Pattern.compile(NamedXmlBeanDefinitionParser.PARENT_BEAN_FACTORY_ELEMENT).matcher(description);
+                //                if (pbfm.find()) {
+                //                    parentName = pbfm.group(1);
+                //                }
+            }
+            if (this.name == null || "".equals(StringUtils.trimAllWhitespace(this.name))) {
+                this.name = this.defaultName;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
